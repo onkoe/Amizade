6cede4393752be2b5830112c7d4d457bbcab7b56
Barrett Ray <onkoe@protonmail.com>
2023-05-13 18:08:06 -0500

    used the Relm4 template as a base
    instead of rawdogging it

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,50 @@
-// TODO: https://github.com/Relm4/relm4-template.git
-// who knows when i'll see this lmao
+#[rustfmt::skip]
+mod config;
+mod app;
+mod modals;
+mod setup;
 
-use gtk::prelude::*;
-use relm4::prelude::*;
+use gtk::prelude::ApplicationExt;
+use relm4::{
+    actions::{AccelsPlus, RelmAction, RelmActionGroup},
+    gtk, main_application, RelmApp,
+};
 
-struct App {
-    counter: u8,
-}
+use app::App;
+use setup::setup;
 
-#[derive(Debug)]
-enum Msg {
-    Increment,
-    Decrement,
-}
+use crate::config::APP_ID;
 
-#[relm4::component]
-impl SimpleComponent for App {
-    type Init = u8;
-    type Input = Msg;
-    type Output = ();
-
-    view! {
-        gtk::Window {
-            set_title: Some("Simple app"),
-            set_default_size: (300, 100),
-
-            gtk::Box {
-                set_orientation: gtk::Orientation::Vertical,
-                set_spacing: 5,
-                set_margin_all: 5,
-
-                gtk::Button {
-                    set_label: "Increment",
-                    connect_clicked => Msg::Increment,
-                },
-
-                gtk::Button {
-                    set_label: "Decrement",
-                    connect_clicked => Msg::Decrement,
-                },
-
-                gtk::Label {
-                    #[watch]
-                    set_label: &format!("Counter: {}", model.counter),
-                    set_margin_all: 5,
-                }
-            }
-        }
-    }
-
-    // Initialize the component.
-    fn init(
-        counter: Self::Init,
-        root: &Self::Root,
-        sender: ComponentSender<Self>,
-    ) -> ComponentParts<Self> {
-        let model = App { counter };
-
-        // Insert the code generation of the view! macro here
-        let widgets = view_output!();
-
-        ComponentParts { model, widgets }
-    }
-
-    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
-        match msg {
-            Msg::Increment => {
-                self.counter = self.counter.wrapping_add(1);
-            }
-            Msg::Decrement => {
-                self.counter = self.counter.wrapping_sub(1);
-            }
-        }
-    }
-}
+relm4::new_action_group!(AppActionGroup, "app");
+relm4::new_stateless_action!(QuitAction, AppActionGroup, "quit");
 
 fn main() {
-    let app = RelmApp::new("relm4.example.simple");
-    app.run::<App>(0);
+    // Enable logging
+    tracing_subscriber::fmt()
+        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::FULL)
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
+    setup();
+
+    let app = main_application();
+    app.set_resource_base_path(Some("/com/belmoussaoui/GtkRustTemplate/"));
+
+    let actions = RelmActionGroup::<AppActionGroup>::new();
+
+    let quit_action = {
+        let app = app.clone();
+        RelmAction::<QuitAction>::new_stateless(move |_| {
+            app.quit();
+        })
+    };
+    actions.add_action(&quit_action);
+
+    app.set_accelerators_for_action::<QuitAction>(&["<Control>q"]);
+
+    app.set_action_group(Some(&actions.into_action_group()));
+
+    let app = RelmApp::with_app(app);
+
+    app.run::<App>(());
 }
